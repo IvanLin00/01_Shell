1fc5f3787589b89a52ff8b45660dbdf78b1e3eef
JimmyLuo <jluo00@stuy.edu>
2019-12-03 11:54:15 -0500

    parse

---
+++ parse.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+char ** parse_args(char * line){
+	char *curr = line;
+	char **tokens = malloc(5 * sizeof(char *));
+	int i = 0;
+	while(curr){
+		tokens[i] = strsep(&curr, " ");		
+		i++;
+	}
+	return tokens;
+}
+
