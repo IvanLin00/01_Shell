0ac7acf84ed515f747636d20fd48e650e5279799
JimmyLuo0 <jluo00@stuy.edu>
2019-12-04 11:50:58 -0500

    jesus exists

--- makefile
+++ makefile
@@ -1,3 +1,15 @@
-all: 
+all: main.o parse.o
+	gcc -o shell main.o parse.o
 
-main:
+main.o: main.c parse.h
+	gcc -c main.c
+
+parse.o: parse.c parse.h
+	gcc -c parse.c
+
+run:
+	./shell "ls -a -l"
+
+clean:
+	rm *.o
+	rm shell
--- parse.c
+++ parse.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "parse.h"
 
 char ** parse_args(char * line){
 	char *curr = line;
 	char **tokens = malloc(5 * sizeof(char *));
 	int i = 0;
 	while(curr){
-		tokens[i] = strsep(&curr, " ");		
+		tokens[i] = strsep(&curr, " ");
 		i++;
 	}
 	return tokens;
 }
-
