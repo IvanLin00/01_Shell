427870d5baf1c6c4ac2d213623ce479ebc2f1f91
Ivan Lin <ilin00@stuy.edu>
2019-12-05 11:31:12 -0500

    fork this

---
+++ functions.h
@@ -0,0 +1,10 @@
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void ls (char *directory_name);
+void echo (char *prompt);
