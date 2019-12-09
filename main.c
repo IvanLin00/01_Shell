a82867114aa425d573d826fcd1cfece9b066925c
JimmyLuo0 <jluo00@stuy.edu>
2019-12-09 11:57:53 -0500

    kekw

--- main.c
+++ main.c
@@ -3,18 +3,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 #include "parse.h"
 
 int main(int argc, char *argv[]){
   char *commandline = argv[1];
 	char **args = parse_args(commandline);
-	printf("testing using ls -a -l\n");
+  if (!strncmp(args[0], "cd", 100)  || !strncmp(args[0], "exit", 100)){
+    printf("%s\n",*args);
+    int stuff = chdir(args[1]);
+    printf("error number: %d %s\n", errno, strerror(errno));
+    return 0;
+  }
   int ffs = fork();
   int *status;
   wait(status);
-  if (args [0] == "cd" || args [0] == "exit"){
-
-  }
   if(!ffs){
     execvp(args[0], args);
   }
