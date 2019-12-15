17a379a7747bc843550b8dbacc057df5580057f7
IvanLin00 <ilin00@stuy.edu>
2019-12-15 15:38:02 -0500

    seg fault

--- main.c
+++ main.c
@@ -11,30 +11,20 @@ int main(int argc, char *argv[]){
 		printf("$");
 	  char commandline[100];
 	  fgets(commandline, 100, stdin);
-<<<<<<< HEAD
-	  char **args = parse_args(commandline, "\n");
-    char **s_args = parse_args(args[0], ";");
-		int num_children = 0;
-	  for(int i = 0; !(s_args[i] == NULL); i++){
-	 	   num_children ++;
-	  }
-	  //not sure about order --> fork first or check for cd/exit first
-	  //char **cargs = parse_args(commandline, " ")
-=======
     char **targs = parse_args(commandline, "\n");
-	  char **args = parse_args(commandline, ";");
-    char **cargs = parse_args(commandline, " ");
- 		if (!strncmp(cargs[0], "exit", 100)){
-			exit(0);
-		}
+	  char **args = parse_args(targs[0], ";");
 		int num_children = 1;
 	  for(int i = 0; !(args[i] == NULL); i++){
-      printf("%s\n",args[i]);
-      printf("%d\n",num_children );
+      //printf("%s\n",args[i]);
+      //printf("%d\n",num_children );
 	 	  num_children ++;
 	  }
-    for(;num_children > 0; num_children--){
-      printf("%d\n",num_children );
+    for(int i = 0;i < num_children; i++){
+      char **cargs = parse_args(args[i], " ");
+      if (!strncmp(cargs[0], "exit", 100)){
+        exit(0);
+      }
+      //printf("%d\n",num_children );
       int ffs = fork();
   		int *status;
   		wait(status);
@@ -42,39 +32,7 @@ int main(int argc, char *argv[]){
   		  execvp(cargs[0], cargs);
   		}
     }
-	// //not sure about order --> fork first or check for cd/exit first
-	  // char **cargs = parse_args(commandline, " ");
->>>>>>> fb7a1081acd9adaabb5b9a0b2a8b219c886ae6db
- 		// if (!strncmp(cargs[0], "exit", 100)){
-		// 	exit(0);
-		// }
-		// if (!strncmp(cargs[0], "cd", 100)){
-		//   chdir(cargs[1]);
-		// }
-    for(int i = 0; i < num_children; i++){
-      char **cargs = parse_args(s_args[i], " ");
-      printf("%s\n", cargs[0]);
-      int f = fork();
-      int *status;
-      wait(status);
-      if(!f) execvp(cargs[0], cargs);
-    }
-		// int ffs = fork();
-		// int *status;
-		// wait(status);
-		// if(!ffs){
-		//   execvp(cargs[0], cargs);
-		// }
-<<<<<<< HEAD
-=======
-		// int ffs = fork();
-		// int *status;
-		// wait(status);
-		// if(!ffs){
-		//   execvp(cargs[0], cargs);
-		// }
->>>>>>> fb7a1081acd9adaabb5b9a0b2a8b219c886ae6db
-  }
+}
 }
 
 //cd, exit, ;, redirection, pipes
